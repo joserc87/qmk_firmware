8db594e3b9d50a31dbdd9fb2d1bc81f3148a718f
Jose Ramon Cano Yribarren <joserc87@gmail.com>
2020-06-20 01:53:24 +0200

    Change right thumg cluster

--- keyboards/dactyl-manuform/keymaps/5x6-joserc87/keymap.c
+++ keyboards/dactyl-manuform/keymaps/5x6-joserc87/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                      KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, 
                      KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSHIFT,
                                       KC_LBRC, KC_RBRC,
-        CTL_T(KC_SPC), LT(_WM, KC_BSPC), 
+        KC_SPC, CTL_T(KC_BSPC), 
         ALT_T(KC_ESC), GUI_T(KC_ESC),
         KC_LALT, KC_LGUI),
 [_FN] = KEYMAP_5x6(
