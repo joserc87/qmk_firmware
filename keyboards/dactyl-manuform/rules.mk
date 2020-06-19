2ae5889d7cf6f769ed7b8c98e52ac7465bd9c54d
Jose Ramon Cano Yribarren <joserc87@gmail.com>
2020-06-20 01:53:51 +0200

    Fix make by adding a sleep

--- keyboards/dactyl-manuform/rules.mk
+++ keyboards/dactyl-manuform/rules.mk
@@ -82,6 +82,7 @@ avrdude: build
 	  ls /dev/tty* > /tmp/2; \
 	  USB=`diff /tmp/1 /tmp/2 | grep -o '/dev/tty.*'`; \
 	done; \
+    sleep 2; \
 	avrdude -p $(MCU) -c avr109 -P $$USB -U flash:w:$(BUILD_DIR)/$(TARGET).hex
 
 .PHONY: avrdude
